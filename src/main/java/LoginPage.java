977f3441a0be3242e930aaa74a3f9069d79b0045
Taras_Sokil <saicll92@gmail.com>
2020-12-22 15:32:13 +0200

    NewProject

---
+++ src/main/java/LoginPage.java
@@ -0,0 +1,36 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class LoginPage {
+
+
+    private WebDriver driver;
+    private By userNameFieldLocator =By.id("user-name");
+    private By userPasswordFieldLocator =By.id("password");
+    private By userLoginButtonLocator =By.className("btn_action");
+
+
+    public LoginPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    private WebElement getUserNameField() {
+        return driver.findElement(userNameFieldLocator);
+    }
+
+    private WebElement getUserPasswordField() {
+        return driver.findElement(userPasswordFieldLocator);
+    }
+
+    private WebElement getLoginButton() {
+        return driver.findElement(userLoginButtonLocator);
+    }
+
+    public void login() {
+        getUserNameField().sendKeys("standard_user");
+        getUserPasswordField().sendKeys("secret_sauce");
+        getLoginButton().click();
+
+    }
+}
